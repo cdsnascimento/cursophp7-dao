6f7c33cc38bf1c1ea14f3f8c97d7f6bb21430b79
cdsnascimento <69284317+cdsnascimento@users.noreply.github.com>
2020-09-26 10:24:04 -0300

    Implementado comando delete
    69. PDO - DAO - DELETE,
    ALtearado o Colltion da tb_usuarios no Banco para utf8_general_ci
    Corrigido erro no método Update. O regsitro em questão não era setado

--- index.php
+++ index.php
@@ -1,38 +1,44 @@
 <?php
 
+
     require_once("config.php");
 
     //exibir todos os funcionarios cadastrados
-    // $sql = new Sql();   
-    // $usuarios = $sql->select("SELECT * FROM tb_usuarios");
-    // echo json_encode($usuarios);
+/*     $sql = new Sql();   
+    $usuarios = $sql->select("SELECT * FROM tb_usuarios");
+    echo json_encode($usuarios); */
 
-    // $root = new Usuario();   
-    // $root->loadById(8);  
-    // echo $root;
+    //Localiza um usuario pelo id
+    $usuario = new Usuario();   
+    $usuario->loadById(11);  
 
-    // $lista = Usuario::getList();
-    // echo json_encode($lista);
+/*     //Lista todos os usuarios
+    $lista = Usuario::getList();
+    echo json_encode($lista); */
 
-    // $search = Usuario::search("roo");
-    // echo json_encode($search);
+/*     //Localiza todos os usuarios pelo nome
+    $search = Usuario::search("roo");
+    echo json_encode($search);
+ */
 
-    // $usuario = new Usuario();
-    // $usuario->login("root","!@#$");
-
-    // echo $usuario;
-
-    // $aluno = new Usuario("Celi","3345");
-    
-    // $aluno->insert();
-
-    // echo $aluno;
-
+/*     //Valida o usuario e a senha
     $usuario = new Usuario();
-    $usuario->loadById(8);
+    $usuario->login("root","!@#$"); */
 
-    $usuario->update("professor", "%%$#");
+/*     //Insere um novo usuario
+    $usuario = new Usuario("Celi","3345");
+    $usuario->insert(); */
 
+    
+    //Atualiza um usuario carregado na memoria / Classe
+    $usuario->update("Conceição", "271255");
+
+    
+/*     
+    //Deleta um usuario
+    $usuario->delete();
+
+ */
     echo $usuario;
 
 ?>
